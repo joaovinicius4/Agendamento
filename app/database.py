0d0360ee842ad0a77687175b8c2ac019835bfe87
joaovinicius4 <joaoruas111@gmail.com>
2026-08-19 17:40:26 -0300

    feat: add tabela agendamento

--- app/database.py
+++ app/database.py
@@ -14,5 +14,20 @@ def initialize_database(connection: sqlite3.Connection) -> None:
         especialidade TEXT NOT NULL
     );
     """
+    ddl_agendamentos = """
+    CREATE TABLE IF NOT EXISTS agendamentos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cliente_nome TEXT NOT NULL,
+        profissional_id INTEGER NOT NULL,
+        data_inicio TEXT NOT NULL,
+        data_fim TEXT NOT NULL,
+        status TEXT NOT NULL DEFAULT 'agendado'
+        CHECK (status IN ('agendado', 'cancelado')),
+        FOREIGN KEY (profissional_id) REFERENCES profissionais(id),
+        CHECK (data_fim > data_inicio)
+    );
+    """
+
     connection.execute(ddl_profissionais)
+    connection.execute(ddl_agendamentos)
     connection.commit()
